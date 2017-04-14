b9a980cb017c2c4e37a5f1b51f86cc70fae3c4ed
dante <dante.casanova@gmail.com>
2017-04-14 01:25:09 -0500

    increment / decrement

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.justjava;
 
+import android.icu.text.NumberFormat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.TextView;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+    int numberOfCoffees = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +22,23 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        displayPrice(numberOfCoffees * 5);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void increment(View view) {
+        numberOfCoffees++;
+        display(numberOfCoffees);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void decrement(View view) {
+        numberOfCoffees--;
+        display(numberOfCoffees);
     }
 
     /**
@@ -29,5 +47,12 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price value on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.price_text_view);
+        quantityTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
